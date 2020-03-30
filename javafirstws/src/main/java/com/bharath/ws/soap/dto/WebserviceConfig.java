80f3625d56412b184ef785a8de4f96158334fed1
Aarthi Ananth <rthi.shini@gmail.com>
2020-03-30 15:30:28 +0530

    Add files via upload

--- javafirstws/src/main/java/com/bharath/ws/soap/dto/WebserviceConfig.java
+++ javafirstws/src/main/java/com/bharath/ws/soap/dto/WebserviceConfig.java
@@ -1,15 +1,22 @@
 package com.bharath.ws.soap.dto;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.xml.ws.Endpoint;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.wss4j.common.ConfigurationConstants;
+import org.apache.wss4j.dom.WSConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.bharath.ws.soap.PaymentProcessorImpl;
+
 
 
 @Configuration
@@ -21,8 +28,15 @@ public class WebserviceConfig {
 	@Bean
 	@SuppressWarnings("resource")
 	public Endpoint endpoint(){
-		Endpoint endpoint=new EndpointImpl(bus,new PaymentProcessorImpl());
+		EndpointImpl endpoint=new EndpointImpl(bus,new PaymentProcessorImpl());
 		endpoint.publish("/PaymentProcessorImpl");
+		
+		Map<String,Object> inprops = new HashMap<>();
+		inprops.put(ConfigurationConstants.ACTION, ConfigurationConstants.USERNAME_TOKEN);
+		inprops.put(ConfigurationConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+		inprops.put(ConfigurationConstants.PW_CALLBACK_CLASS,UTPasswordCallback.class.getName());
+		WSS4JInInterceptor wssjn=new WSS4JInInterceptor(inprops);
+		endpoint.getInInterceptors().add(wssjn);
 		return endpoint;
 		
 	}
